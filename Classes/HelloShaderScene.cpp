8752f77c6047adcaf3c46a0d3b13af5c12505bbb
fujisawa <sfujisawa2019@gmail.com>
2020-01-27 11:25:50 +0900

    FBXファイルを変換したモデルを表示

--- Classes/HelloShaderScene.cpp
+++ Classes/HelloShaderScene.cpp
@@ -93,10 +93,10 @@ bool HelloShader::init()
 	//node->setContentSize(Size(500, 500));
 	//this->addChild(node, 1);
 
-	m_pSprite3D = Sprite3D::create("orc/orc.c3b");
+	m_pSprite3D = Sprite3D::create("knight/knight.c3b");
 	m_pSprite3D->setPosition(640, 360);
-	m_pSprite3D->setScale(10.0f);
-	m_pSprite3D->setRotation3D(Vec3(0, 180, 0));
+	//m_pSprite3D->setScale(10.0f);
+	//m_pSprite3D->setRotation3D(Vec3(0, 180, 0));
 	this->addChild(m_pSprite3D, 1);
 
 	Animation3D* animation = Animation3D::create("orc/orc.c3t");
